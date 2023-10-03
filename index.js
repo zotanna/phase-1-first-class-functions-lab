32a048c033f0ac26de2566344b17d2ed56a440b9
denis <denniiskim@gmail.com>
2023-10-03 04:43:58 +0300

    Completed first class assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  const returnLastTwoDrivers = function(drivers) {
+    const length = drivers.length;
+    return drivers.slice(length - 2, length);
+  };
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+
+  function selectDifferentDrivers(drivers, selectingFunction) {
+    return selectingFunction(drivers);
+  }
+  
